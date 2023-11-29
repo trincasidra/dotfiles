6f3627b1214f47fd842f90c4fed1f2e1a7061eb8
Robb <15145418+trincasidra@users.noreply.github.com>
2023-11-29 01:43:47 +0100

    Update install_aur_packages.sh

--- install_aur_packages.sh
+++ install_aur_packages.sh
@@ -1,2 +1,5 @@
 #!bin/bash
-yay dracula-cursors-git dracula-icons-git dracula-theme-gtk refind-theme-dracula
+yay --answerdiff None --answerclean All --removemake dracula-cursors-git
+yay --answerdiff None --answerclean All --removemake dracula-icons-git
+yay --answerdiff None --answerclean All --removemake dracula-theme-gtk
+yay --answerdiff None --answerclean All --removemake refind-theme-dracula
