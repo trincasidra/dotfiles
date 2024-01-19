7c9be03592007a80d15a5886d1fc2b57e98d1085
Robb <15145418+trincasidra@users.noreply.github.com>
2024-01-19 11:49:35 +0100

    Fix fjira AUR and add color

--- scripts/install_aur_packages_work.sh
+++ scripts/install_aur_packages_work.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-paru --skipreview --removemake --noconfirm -S fjira
+paru --skipreview --removemake --noconfirm -S fjira-bin
 paru --skipreview --removemake --noconfirm -S google-chrome
 paru --skipreview --removemake --noconfirm -S slack-desktop
