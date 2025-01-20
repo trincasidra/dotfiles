4bb16012046f01a583b6ba71903673781c7fdbff
Robb <15145418+trincasidra@users.noreply.github.com>
2025-01-20 21:30:50 +0100

    Change flameshot with grimblast for now

--- scripts/install_aur_packages_base.sh
+++ scripts/install_aur_packages_base.sh
@@ -6,3 +6,4 @@ paru --skipreview --removemake --noconfirm -S dracula-gtk-theme
 paru --skipreview --removemake --noconfirm -S dracula-icons-git
 paru --skipreview --removemake --noconfirm -S refind-theme-dracula
 paru --skipreview --removemake --noconfirm -S hyprcursor-dracula-kde-git
+paru --skipreview --removemake --noconfirm -S grimblast-git
