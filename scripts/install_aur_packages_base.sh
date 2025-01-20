6fc83c28311ffa39f80a6a089fd31f0c6bb08256
Robb <15145418+trincasidra@users.noreply.github.com>
2025-01-20 14:48:36 +0100

    Change to hyprland

--- scripts/install_aur_packages_base.sh
+++ scripts/install_aur_packages_base.sh
@@ -1,12 +1,7 @@
 #!bin/bash
 paru --skipreview --removemake --noconfirm -S cava
 paru --skipreview --removemake --noconfirm -S checkupdates-with-aur
-paru --skipreview --removemake --noconfirm -S clipit
 paru --skipreview --removemake --noconfirm -S dracula-cursors-git
 paru --skipreview --removemake --noconfirm -S dracula-gtk-theme
 paru --skipreview --removemake --noconfirm -S dracula-icons-git
-paru --skipreview --removemake --noconfirm -S ghcup-hs-bin
-paru --skipreview --removemake --noconfirm -S nuclear-player-bin
 paru --skipreview --removemake --noconfirm -S refind-theme-dracula
-paru --skipreview --removemake --noconfirm -S sc-it
-paru --skipreview --removemake --noconfirm -S wordgrinder
--- scripts/install_packages_home.sh
+++ scripts/install_packages_home.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-doas -n pacman -Syu --noconfirm discord lutris mgba-qt
+doas -n pacman -Syu --noconfirm discord steam lutris mgba-qt
