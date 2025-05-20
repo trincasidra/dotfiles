8110c59f561cc2b36e88a885aa2e00b3b0921ac4
Robb <15145418+trincasidra@users.noreply.github.com>
2025-05-20 11:24:03 +0200

    Update install_aur_packages_base.sh

--- scripts/install_aur_packages_base.sh
+++ scripts/install_aur_packages_base.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 paru --skipreview --removemake --noconfirm -S cava
-paru --skipreview --removemake --noconfirm -S dracula-cursors-git
-paru --skipreview --removemake --noconfirm -S dracula-gtk-theme
+paru --skipreview --removemake --noconfirm -S full-dracula-theme-git
 paru --skipreview --removemake --noconfirm -S dracula-icons-git
 paru --skipreview --removemake --noconfirm -S hyprcursor-dracula-kde-git
 paru --skipreview --removemake --noconfirm -S hyprsysteminfo
