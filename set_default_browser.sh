efff5981144a5cbe12ed4d981c749810b1d4f4c8
Robb <15145418+trincasidra@users.noreply.github.com>
2025-05-20 10:42:37 +0200

    Delete set_default_browser.sh

--- set_default_browser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
-xdg-mime default org.qutebrowser.qutebrowser.desktop x-scheme-handler/http
-xdg-mime default org.qutebrowser.qutebrowser.desktop scheme-handler/http
-xdg-mime default org.qutebrowser.qutebrowser.desktop x-scheme-handler/https
-xdg-mime default org.qutebrowser.qutebrowser.desktop scheme-handler/https
