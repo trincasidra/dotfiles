f8cc66dd9403e076ae2606bb88d79f4865078ac0
Robb <15145418+trincasidra@users.noreply.github.com>
2025-05-20 10:30:02 +0200

    Update install_home.sh

--- install_home.sh
+++ install_home.sh
@@ -1,5 +1,16 @@
 #!bin/bash
-sh scripts/enable_multilib.sh
+doas sh scripts/enable_multilib.sh
+echo "GPU brand?"
+echo "  1) AMD Radeon"
+echo "  2) Nvidia"
+echo "  3) Intel" <br/>
+read n
+case $n in
+  1) sh scripts/install_radeon.sh;;
+  2) sh scripts/install_nvidia.sh;;
+  3) sh scripts/install_intel.sh;;
+  *) echo "Invalid option, skipping...";;
+esac
 sh scripts/install_packages_base.sh
 sh scripts/install_packages_home.sh
 sh scripts/install_paru.sh
