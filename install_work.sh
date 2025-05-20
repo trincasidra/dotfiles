6d927a9206e8e9d47513c53c3dd5ed785c75f967
Robb <15145418+trincasidra@users.noreply.github.com>
2025-05-20 10:41:12 +0200

    Update install_work.sh

--- install_work.sh
+++ install_work.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 sh scripts/install_packages_base.sh
+sh scripts/enable_services.sh
 sh scripts/install_packages_work.sh
 sh scripts/install_paru.sh
 sh scripts/install_aur_packages_base.sh
